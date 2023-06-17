3ce1ecc18ea980231f44ac62af11e7c67a59504b
regina <reggiekariuki58@gmail.com>
2023-06-17 10:36:14 +0300

    create

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+wrap_parameters format:[]
   # GET /birds
   def index
     birds = Bird.all
@@ -15,5 +15,14 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  def create 
+    bird = Bird.create(bird_params)
+    render json: bird
+  end
+  private
+    def bird_params
+      params.permit(:name,:species)
+    end
+
 
 end
